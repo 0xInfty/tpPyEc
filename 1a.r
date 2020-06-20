bb442f4c5650c4380ce4acb4e86f3922a5c69da7
Ezequielk <ezequielginsberg@gmail.com>
2020-06-20 16:37:39 -0300

    Agrego comparación con densidad uniforme en el hist

--- 1a.r
+++ 1a.r
@@ -15,3 +15,5 @@ hist(x1...x1000,
      xlab = "Muestra x1...x1000", 
      ylab = "Densidad",
      col = "#66B2FF")
+abline(h=1,col="red") # Comparo con la densidad de una U[0,1]
+text(0.25,1.05,"f(x)=1/(1-0)=1", col="red") # (si conoces una mejor forma de presentar a la densidad teórica en el gráfico mejor, porque queda medio feo esto)
